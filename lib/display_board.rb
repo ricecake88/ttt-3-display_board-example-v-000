1215c26e85a7ca38af7b66d779fc8a506f391c51
Grace Shih <vocalise@gmail.com>
2018-04-20 21:41:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  print "A Tic Tac Toe Board\n"
+  line1 = "     " + "|" + "     " + "|" + "     \n"
+  line2 = "-----------\n"
+  print line1
+  print line2
+  print line1
+  print line2
+  print line1
+  print line2
+end
