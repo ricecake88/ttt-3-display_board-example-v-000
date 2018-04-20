5a1e747b764f65526a9e48fd9b361876b6bd0db0
Grace Shih <vocalise@gmail.com>
2018-04-20 21:51:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,10 @@
 def display_board
   print "A Tic Tac Toe Board\n"
   line1 = "   |   |   \n"
-  line2 = "-----------\n"
-  print line1
-  print line2
-  print line1
-  print line2
-  print line1
+  line1 += "-----------\n"
+  line1 = "   |   |   \n"
+  line1 = "   |   |   \n"  
+  
 end
 
 display_board
